88c3f2f4c6ebd520d4be9845ff3d940986b4ac53
Gabbe2003 <gabriell.touma@gmail.com>
2023-12-01 00:20:32 +0100

    Done

--- index.js
+++ index.js
@@ -3,13 +3,11 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); 
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -30,6 +28,6 @@ app.get('/api/whoami', (req, res) => {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
